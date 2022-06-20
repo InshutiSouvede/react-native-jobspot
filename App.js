ac7b06f8310478f750f2f6aed686d7fc0b0b07ca
joycyrus <isouvede2@gmail.com>
2022-06-20 21:58:46 +0200

    adding stack navigation

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from './screens/SplashScreen';
+import LoginScreen from './screens/LoginScreen';
+import SignUpScreen from './screens/SignUpScreen';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen component={SplashScreen} name="splash" />
+        <Stack.Screen component={LoginScreen} name="login" />
+        <Stack.Screen component={SignUpScreen} name="signup" />
+      </Stack.Navigator>
+    </NavigationContainer>    
   );
 }
 
